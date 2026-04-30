a74b65b45dac1cf1f8c00602a02b199ea66d230d
ArdiMobile <34771129+ArdiMobile@users.noreply.github.com>
2026-04-30 15:57:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const urlInput = document.getElementById('urlInput');
 const preview = document.getElementById('preview');
 
 // 👉 CHANGE THIS to your Railway URL
-const API_URL = "https://newfb.up.railway.app";
+const API_URL = "https://newfb-3igr.onrender.com";
 
 async function processDownload(url) {
     preview.innerHTML = `
